58475e90c11d15b4ba653e71cf169e96f3bea97d
Mathias Aleksander Emberland Østby <maostby@student.matnat.uio.no>
2023-06-21 16:18:10 +0200

    Initialization and skeleton for project

---
+++ main.py
@@ -0,0 +1,15 @@
+# Author: Mathiasoestby
+# Created: 21.06.2023
+# 
+
+def initilazation():
+    """Setup for the game"""
+    pass
+
+def drawing():
+    """Method for drawing game GUI"""
+    pass
+
+def logic():
+    """The logic for the game"""
+    pass
